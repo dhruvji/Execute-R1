fde0bdb25f566b7c7cb95c2d57b171124c8c9c45
dhruvji <dhruvgautam@berkeley.edu>
2025-03-01 11:13:58 +0000

    reward changed i think

--- verl/trainer/main_ppo.py
+++ verl/trainer/main_ppo.py
@@ -17,13 +17,15 @@ Note that we don't combine the main with ray_trainer as ray_trainer is used by o
 
 from verl import DataProto
 import torch
-from verl.utils.reward_score import qa_em
+from verl.utils.reward_score import qa_em, execute
 from verl.trainer.ppo.ray_trainer import RayPPOTrainer
 import re
 import numpy as np
 
 def _select_rm_score_fn(data_source):
-    if "nq" in data_source:
+    if "execute" in data_source:
+        return execute.compute_score 
+    elif "nq" in data_source:
         return qa_em.compute_score_em
     else:
         raise NotImplementedError
